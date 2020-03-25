54990fc58aacb14686c45dfe0dfbc8827cedea50
rafa <ralomach@gmail.com>
2020-03-25 17:01:10 -0300

    Remover origin do cors do back-end

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,8 +4,10 @@ const routes = require('./routes');
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost'}));
+app.use(cors());
 app.use(express.json()); 
 app.use(routes);
+
+console.log('Escutando solicitações...');
 
 app.listen(3333);
